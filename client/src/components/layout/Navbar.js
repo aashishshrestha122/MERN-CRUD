f754e194d8d5062bb489e0990aafa214f0ac6d4a
Aashish <aashishshrestha12@gmail.com>
2020-08-24 15:10:57 +0545

    login and signup form added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import "../../App.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-dark">
-      <h1>
-        <a href="index.html">
-          <i className="fas fa-code"></i> DevConnector
-        </a>
-      </h1>
-      <ul>
-        <li>
-          <a href="profiles.html">Developers</a>
-        </li>
-        <li>
-          <a href="register.html">Register</a>
-        </li>
-        <li>
-          <a href="login.html">Login</a>
-        </li>
-      </ul>
-    </nav>
+    <Fragment>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <h1>
+          <Link to="/">
+            <i className="fas fa-code"></i> MERN
+          </Link>
+        </h1>
+        <ul>
+          <li>
+            <a href="profiles.html">Developers</a>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </Fragment>
   );
 };
 
